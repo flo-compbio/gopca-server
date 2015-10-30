ab7b3db4832b6578e99e5033541a042cee6bd883
Florian Wagner <florian.wagner@duke.edu>
2015-10-30 19:59:35 -0400

    fix

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='gopca-server',
 
-    version='0.1',
+    version='0.1rc',
 
     description=description,
     long_description=long_description,
